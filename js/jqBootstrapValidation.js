0dbbebfbaa37a2ba516209939c3ec71b270e3999
AurelL69 <aurelien.landelle@gmail.com>
2016-05-29 10:40:38 +0200

    ajout infos modif contact +last guest + programme

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -106,7 +106,7 @@
             //                                                   PATTERN
             // ---------------------------------------------------------
             if ($this.attr("pattern") !== undefined) {
-              message = "Not in the expected format<!-- data-validation-pattern-message to override -->";
+              message = "¨Ce n'est pas le bon format<!-- data-validation-pattern-message to override -->";
               if ($this.data("validationPatternMessage")) {
                 message = $this.data("validationPatternMessage");
               }
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,8 +17,8 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'aurelien.landelle@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "Vous avez reçu un nouveau message en provenance du site :\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Contact - Département Français :  $name";
+$email_body = "Vous avez reçu un nouveau message en provenance du site du Département de Français.\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
